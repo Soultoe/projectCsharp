3866b8e43a091e4b9341f0ef684014bdb8a8228e
RomainB <romainbrisse@live.fr>
2019-04-01 16:30:14 +0200

    listening thread in client

--- clientApplication/clientApplication/Program.cs
+++ clientApplication/clientApplication/Program.cs
@@ -30,7 +30,11 @@ namespace clientApplication
             username = Console.ReadLine();
 
             Connect(server,"hello! I'm a client!",username);
-            
+
+            //new thread to listen to incoming messages --> ReceiveMsg
+            Thread receive = new Thread(ReceiveMsg);
+            receive.Start();
+
             do
             {
                 message = Console.ReadLine();
@@ -101,12 +105,17 @@ namespace clientApplication
             String data = String.Empty;
             int i;
 
-            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-            {
-                // Translate data bytes to a ASCII string.
-                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                Console.WriteLine("{0}", data);
-            }
+            //while (true)
+            //{
+
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("{0}", data);
+                    data = String.Empty;
+                }
+            //}
         }
 
         static void Disconnect()
